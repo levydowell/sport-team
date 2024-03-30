f25d31112d541875bdacfcd6a5b0b57893618683
levydowell <33126924+levydowell@users.noreply.github.com>
2024-03-30 13:00:00 -0600

    initial commit

---
+++ app/roster/page.js
@@ -0,0 +1,10 @@
+
+export default function Roster() {
+	return (
+		<main>
+			<div>
+				<h1>Welcome to the Roster Page</h1>
+			</div>
+		</main>
+	);
+}
---
+++ app/stats/page.js
@@ -0,0 +1,9 @@
+export default function Stats() {
+	return (
+		<main>
+			<div>
+				<h1>Welcome to the Stats Page</h1>
+			</div>
+		</main>
+	);
+}
